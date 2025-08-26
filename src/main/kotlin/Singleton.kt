7d669f7ba429a0ef575b6576e6d8610c1422166d
Chaosgh <149420543+Chaosgh@users.noreply.github.com>
2025-08-27 00:22:37 +0200

    Clamp vision field of view

--- src/main/kotlin/Singleton.kt
+++ src/main/kotlin/Singleton.kt
@@ -3,16 +3,20 @@ package de.chaos
 
 import com.typewritermc.core.extension.Initializable
 import com.typewritermc.core.extension.annotations.Singleton
-
+import org.bukkit.Bukkit
+import org.bukkit.event.HandlerList
 
 @Singleton
 object Initializer : Initializable {
 
-    override suspend fun initialize() {
+    private val listeners = listOf(PlayerSeenListener)
 
+    override suspend fun initialize() {
+        val plugin = Bukkit.getPluginManager().getPlugin("Typewriter") ?: return
+        listeners.forEach { Bukkit.getPluginManager().registerEvents(it, plugin) }
     }
 
     override suspend fun shutdown() {
-
+        listeners.forEach { HandlerList.unregisterAll(it) }
     }
 }
